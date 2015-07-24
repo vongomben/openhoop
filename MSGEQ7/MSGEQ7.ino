4c1710ab4d15bb0fbb0a4cf9303a601ef72a4188
vongomben <davide.gomba@gmail.com>
2015-07-24 17:48:01 +0200

    changed pin mapping in code

--- MSGEQ7/MSGEQ7.ino
+++ MSGEQ7/MSGEQ7.ino
@@ -1,26 +1,21 @@
-//  https://www.sparkfun.com/datasheets/Components/General/MSGEQ7.pdf
+/*
+    Basic MSGEQ7 Serial Out 
+    https://www.sparkfun.com/datasheets/Components/General/MSGEQ7.pdf
+    
+*/
 
 #define msg7RESET 9
 #define msg7Strobe A0
 #define msg7DCout A5
-//const int LEDpins[7] = {3,5,5,6,9,9,10};    // there are 5 LEDs and 7 freq bands.  So, repeat LEDs
- 
-//#define pushButton 2
  
 int msg7bands[7];
  
 void setup() {
   Serial.begin(115200);
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  //for (int x=0; x<7; x++) {
-  //    pinMode(LEDpins[x], OUTPUT);
- // }
+
   pinMode(msg7RESET, OUTPUT);
   pinMode(msg7Strobe, OUTPUT);
  
-  //pinMode(pushButton, INPUT);       // never actually used in this example.
-  //digitalWrite(pushButton, HIGH);  // Enable internal pull-up
 }
  
 void loop() {
@@ -37,13 +32,8 @@ void loop() {
          Serial.print(spectrumRead);
          Serial.print(" ");
 
-        //int PWMvalue = map(spectrumRead, 0, 1024, 0, 255);  // scale analogRead's value to Write's 255 max
-        //if (PWMvalue < 50)
-        //    PWMvalue = PWMvalue / 2;        // bit of a noise filter, so the LEDs turn off at low levels
- 
-        //analogWrite(LEDpins[x], PWMvalue);
-        digitalWrite(msg7Strobe, HIGH);
-        //delay(1000);
+         digitalWrite(msg7Strobe, HIGH);
+
     }
     Serial.println();
     delay(10);
